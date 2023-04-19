fe26c76588462697893a08ffe4917e66aa12b353
Oguro Haruka <Oguro Haruka@MSI>
2023-04-19 16:26:59 +0900

    00_04

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <Windows.h>
-#include<cstdint>
+#include <cstdint>
+#include <string>
+#include <format>
 
 LRESULT CALLBACK WindowsProc(HWND hwnd, UINT msg,
 	WPARAM wparam, LPARAM lparam) {
@@ -9,6 +11,44 @@ LRESULT CALLBACK WindowsProc(HWND hwnd, UINT msg,
 		return 0;
 	}
 	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+void log(const std :: string & message) {
+	OutputDebugStringA(message. c_str());
+}
+
+std::wstring ConvertString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str)
+{
+	if (str.empty())
+	{
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
 }
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -52,6 +92,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		}
 	}
+
+	std::wstring ConvertString(const std::string & str);
+	std::string ConvertString(const std::wstring & str);
 	
 	return 0;
 
