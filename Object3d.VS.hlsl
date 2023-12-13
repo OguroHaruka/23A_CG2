2cc1968f9f9b70fe965d1af0c4131ee4fc0a4547
Oguro Haruka <Oguro Haruka@MSI>
2023-12-13 12:22:40 +0900

    a

--- Object3d.VS.hlsl
+++ Object3d.VS.hlsl
@@ -9,7 +9,7 @@ struct VertexShaderInput {
 	float32_t4 position : POSITION0;
 	float32_t2 texcoord : TEXCOORD0;
 };
-VertexShaderOutput main(VertexShaderInput input,uint32_t instanceId : SV_InstanceID) {
+VertexShaderOutput main(VertexShaderInput input) {
 
 	VertexShaderOutput output;
 	output.position = Mul(input.position,gTransformationMatrices[instanceId].WVP);
